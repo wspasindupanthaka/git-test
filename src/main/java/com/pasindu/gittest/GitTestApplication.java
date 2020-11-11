fe05dca2630ee092ba83c0eab12ae63c721adcc3
wspasindupanthaka <wpasindupanthaka@gmail.com>
2020-11-11 15:17:27 +0530

    commit to main branch

--- src/main/java/com/pasindu/gittest/GitTestApplication.java
+++ src/main/java/com/pasindu/gittest/GitTestApplication.java
@@ -3,7 +3,7 @@ package com.pasindu.gittest;
 public class GitTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Development");
+		System.out.println("Main");
 	}
 
 }
