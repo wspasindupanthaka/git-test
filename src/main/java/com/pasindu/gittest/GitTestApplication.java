446d2fca5a02837506a96b8a5b53f147a7c7208a
wspasindupanthaka <wpasindupanthaka@gmail.com>
2020-11-19 14:09:58 +0530

    commit to my feature

--- src/main/java/com/pasindu/gittest/GitTestApplication.java
+++ src/main/java/com/pasindu/gittest/GitTestApplication.java
@@ -10,6 +10,7 @@ public class GitTestApplication {
 		System.out.println("Feature04");
 		System.out.println("Feature05");
 		System.out.println("Feature06");
+		System.out.println("my feature 1");
 	}
 
 }
